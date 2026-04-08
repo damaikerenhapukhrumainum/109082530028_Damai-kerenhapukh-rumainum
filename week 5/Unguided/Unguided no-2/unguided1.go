0301b84728cc7fae27b1b4a4052ec4c6bd56935b
damaikerenhapukhrumainum <kerenhapukhdamai@gmail.com>
2026-04-08 16:40:15 +0700

    week 5-modul 5_109082530028

---
+++ week 5/Unguided/Unguided no-2/unguided1.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+
+func pola(n int, i int) {
+	if i > n {
+		return
+	}
+
+	for j := 0; j < i; j++ {
+		fmt.Print("*")
+	}
+	fmt.Println()
+
+	pola(n, i+1)
+}
+
+func main() {
+	var n int
+	fmt.Print("Masukkan nilai n: ")
+	fmt.Scan(&n)
+
+	pola(n, 1)
+}
